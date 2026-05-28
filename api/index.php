879f210fe674cabe22ca13175cd959024a231811
Azriel <azriel@example.com>
2026-05-28 13:52:21 +0700

    Fix vercel storage path for compiled views

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
 <?php
+$compiledViewPath = '/tmp/storage/framework/views';
+if (!is_dir($compiledViewPath)) {
+    mkdir($compiledViewPath, 0777, true);
+}
+$_ENV['VIEW_COMPILED_PATH'] = $compiledViewPath;
+putenv("VIEW_COMPILED_PATH={$compiledViewPath}");
+
 require __DIR__ . '/../public/index.php';
